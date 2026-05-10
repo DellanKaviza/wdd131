5759a5765aebebc5a7db2c4980589cb686539ee3
DellanKaviza <82717205+DellanKaviza@users.noreply.github.com>
2026-05-10 08:45:32 +0200

    n

--- scripts/temples.js
+++ scripts/temples.js
@@ -17,4 +17,4 @@ const hambutton = document.querySelector('#menu');
 hambutton.addEventListener('click', () => {
     mainnav.classList.toggle('show');
     hambutton.classList.toggle('show');
-})
+});
