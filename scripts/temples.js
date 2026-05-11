877e139ec8234888914547ed382fd077cd00c720
DellanKaviza <82717205+DellanKaviza@users.noreply.github.com>
2026-05-11 16:05:55 +0200

    n

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,4 +1,6 @@
 // select the DOM elements for output
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
 const year = document.querySelector("#year");
 
 
@@ -9,4 +11,9 @@ document.getElementById("lastModified").innerHTML = document.lastModified;
 const today = new Date();
 
 year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
 
